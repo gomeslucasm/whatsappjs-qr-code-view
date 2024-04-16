4ad400d4b4b2936dfb0237c5c17887dd81c04b6f
Lucas Muller Gomes <lmgomes96@gmail.com>
2024-04-15 23:10:18 -0300

    some changes

--- example/index.js
+++ example/index.js
@@ -1,11 +1,9 @@
 const { Client } = require("whatsapp-web.js");
-const { runQRCodeServer, registerQRCodeRoute } = require("../server");
+const { runQRCodeServer, registerQRCodeRoute } = require("..");
 
 function run() {
-  // Create a new client instance
   const client = new Client();
 
-  // When the client is ready, run this code (only once)
   client.once("ready", () => {
     console.log("Client is ready!");
   });
